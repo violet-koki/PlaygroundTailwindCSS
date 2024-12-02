68229daeef660c3b8c3b894975542379a2ade281
k.yasumoto <yasumotoke_0808@yahoo.co.jp>
2024-12-02 10:57:23 +0900

    feat: font-awesomeのリンク、ハンバーガーメニューの追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
     extend: {
       colors: {
         body: '#050505',
-        white: '#ffffff'
+        white: '#ffffff',
+        "selected-text": "#A3A3FF",
+        thema: "#5c318c"
       },
     },
     fontFamily: {
